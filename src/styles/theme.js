47fd1413bf88eddf026776915c02c6e3ffc255f0
JLisarte <juliano.lisarte@gmail.com>
2024-01-25 17:30:49 -0300

    Add Files

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#3E3B47",
+
+    WHITE: "#F4EDE8",
+    SALMON: "#FF859B",
+
+    GRAY_100: "#999591",
+    GRAY_100: "#666360",
+  },
+}
